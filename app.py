dadd0298681d98967b602d4dd343737048b57ac2
kdmanwani <kdmanwani@gmail.com>
2022-08-17 12:41:21 -0400

    dashboard files

--- app.py
+++ app.py
@@ -12,15 +12,8 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 #Load file
-train1=pd.read_csv('train1.csv')
-train2=pd.read_csv('train2.csv')
-train3=pd.read_csv('train3.csv')
-train4=pd.read_csv('train4.csv')
-train5=pd.read_csv('train5.csv')
-train6=pd.read_csv('train6.csv')
+df=pd.read_csv('sample.csv')
 
-#concate files
-df=pd.concat([train1,train2,train3,train4,train5,train6],ignore_index=True)
 
 markdown_text = ''' This dashboard has some visualizations for exploratory data analysis on Fraudulant credit card
 transactions '''
